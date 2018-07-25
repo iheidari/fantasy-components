d00dbf46476751ea06c77a5fca2d7268c62f348f
Iman Heidari <iman.heidari@gmail.com>
2018-07-24 23:43:34 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import CheckboxDemo from './components/checkbox/checkboxDemo'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CheckboxDemo></CheckboxDemo>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/checkbox/checkboxDemo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Checkbox from './checkbox';
+
+const CheckboxDemo = () => {
+    return (
+        <div>
+            <Checkbox id='chk1' text='Checkbox one'></Checkbox>
+            <Checkbox id='chk2' text='Checkbox two'></Checkbox>
+            <Checkbox id='chk3' text='Checkbox three'></Checkbox>
+        </div>
+    );
+};
+
+export default CheckboxDemo;
